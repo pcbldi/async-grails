ed34d9cbce1a9a4b6e18b20f03396d8af80e9076
prakash23 <prakash@intelligrape.com>
2015-02-28 08:31:59 +0530

    Added async request controller.

---
+++ grails-app/controllers/asyncsample/ReqController.groovy
@@ -0,0 +1,22 @@
+package asyncsample
+
+class ReqController {
+
+    def index() {}
+
+    def context(Long id) {
+        log.info("Inside action: context.")
+        if(id < 1000) {
+            log.info("Done Quickly")
+            render "Done Quickly"
+        } else {
+            def ctx = startAsync()
+            ctx.start {
+                Thread.sleep(loops)
+                log.info("Long Running")
+                render "Long Running"
+                ctx.complete()
+            }
+        }
+    }
+}
